f329f712ef1e7eea959d66b14457145ee3e105d8
chasevanblair <61804205+chasevanblair@users.noreply.github.com>
2022-02-28 19:55:29 -0600

    vehicle Class get/set

--- CarDealership/src/model/Vehicle.java
+++ CarDealership/src/model/Vehicle.java
@@ -8,8 +8,18 @@ package model;
 //vehicle commit test
 public class Vehicle {
 
+	//truck, coupe, sports, sedan, suv, compact etc
+	//could be implemented via a menu so user doesn't choose an unnecessary type of car
 	private String type;
 	
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
 	public Vehicle() {
 		super();
 	}
