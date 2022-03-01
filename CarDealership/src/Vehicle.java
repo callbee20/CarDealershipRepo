e42fb82ff7dbce851c8de7b658b2f8bb4b1e619e
chasevanblair <61804205+chasevanblair@users.noreply.github.com>
2022-02-28 19:28:32 -0600

    changed location of src folders as they were separated from the project

---
+++ CarDealership/src/Vehicle.java
@@ -0,0 +1,19 @@
+
+/**
+ * @author Carson Allbee callbee
+ *CIS175 - Fall 2021
+ * Feb 23, 2022
+ */
+public class Vehicle {
+
+	private String type;
+	
+	public Vehicle() {
+		super();
+	}
+	
+	public Vehicle(String type) {
+		this.type = type;
+	}
+	
+}
