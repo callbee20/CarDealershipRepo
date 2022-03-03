54ba27afed956b576a58d13844a5e66e40a15aa9
chasevanblair <61804205+chasevanblair@users.noreply.github.com>
2022-03-02 20:50:26 -0600

    starting entities and tables with VehicleModel, changed addedToLot to LocalDate datatype. persistence.xml set up, you will have to change connection to your own password and make a database called 'cardealership'

---
+++ CarDealership/src/FillDB.java
@@ -0,0 +1,23 @@
+/**
+ * @author Chase Van Blair - crvanblair
+ * CIS175 - Fall 2021
+ * Mar 2, 2022
+ */
+import model.VehicleModel;
+import controller.VehicleModelHelper;
+
+import java.time.LocalDate;
+
+
+
+public class FillDB {
+	//test values
+	//database cardealership
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		VehicleModel vm = new VehicleModel("ford", "fusion", "rwd", 15000, LocalDate.now());
+		VehicleModelHelper vmh = new VehicleModelHelper();
+		
+		vmh.addModel(vm);
+	}
+}
---
+++ CarDealership/src/controller/VehicleModelHelper.java
@@ -0,0 +1,27 @@
+/**
+ * @author Chase Van Blair - crvanblair
+ * CIS175 - Fall 2021
+ * Mar 2, 2022
+ */
+package controller;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import model.VehicleModel;
+
+public class VehicleModelHelper {
+	static EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("CarDealership");
+
+	public VehicleModelHelper() {
+		
+	}
+	
+	public void addModel(VehicleModel vm) {
+		EntityManager em = emfactory.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(vm);
+		em.getTransaction().commit();
+		em.close();
+	}
+}
